0b9405a7e3fc61aea9efa16ac4bfa559d4b862a3
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-04-02 17:40:41 -0400

    Add feature-tab functionality

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
+const featureButtons = document.querySelectorAll(".feature-btn");
+const featureTabs = document.querySelectorAll(".feature-tab");
 const faqQuestions = document.querySelectorAll(".faq-question");
+
+let activeFeature = 0;
+
+featureButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    featureButtons.forEach((button) => button.classList.remove("active"));
+    featureTabs.forEach((tab) => tab.classList.remove("active"));
+    activeFeature = index;
+    featureButtons[activeFeature].classList.add("active");
+    featureTabs[activeFeature].classList.add("active");
+  });
+});
 
 faqQuestions.forEach((question) => {
   question.addEventListener("click", () => {
