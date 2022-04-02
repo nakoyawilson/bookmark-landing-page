a77805ab2a6954717c14e7e65caf842ed76f8965
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-04-02 19:53:31 -0400

    Add mobile menu functionality

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
+const navigation = document.getElementById("navigation");
+const openButton = document.getElementById("open-menu");
+const closeButton = document.getElementById("close-menu");
+const navLinks = document.querySelectorAll(".nav-link");
 const featureButtons = document.querySelectorAll(".feature-btn");
 const featureTabs = document.querySelectorAll(".feature-tab");
 const faqQuestions = document.querySelectorAll(".faq-question");
 
 let activeFeature = 0;
+
+const openMenu = () => {
+  navigation.classList.add("open");
+  document.body.classList.add("lock-scroll");
+};
+
+const closeMenu = (e) => {
+  navigation.classList.remove("open");
+  document.body.classList.remove("lock-scroll");
+  e.preventDefault();
+};
+
+openButton.addEventListener("click", openMenu);
+closeButton.addEventListener("click", closeMenu);
+navLinks.forEach((link) => link.addEventListener("click", closeMenu));
 
 featureButtons.forEach((button, index) => {
   button.addEventListener("click", () => {
